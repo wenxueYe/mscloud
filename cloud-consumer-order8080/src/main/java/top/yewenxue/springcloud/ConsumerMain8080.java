ffefd6927fdcd8872f524653b043cd91f604d7f0
wenxueYe <yewenxuewy@163.com>
2021-07-20 09:58:16 +0800

    添加了Ribbon支持

--- cloud-consumer-order8080/src/main/java/top/yewenxue/springcloud/ConsumerMain8080.java
+++ cloud-consumer-order8080/src/main/java/top/yewenxue/springcloud/ConsumerMain8080.java
@@ -3,9 +3,13 @@ package top.yewenxue.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import top.yewenxue.myrule.MySelfRule;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration= MySelfRule.class)
+
 public class ConsumerMain8080 {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerMain8080.class,args);
